b3e5e719ef93aa94d9b77f3199c40453e1794342
Vincent Friberg <a12vinfr@student.his.se>
2014-05-16 11:46:23 +0200

    SKITFLYG

--- Assets/_Scripts/FootSteps.cs
+++ Assets/_Scripts/FootSteps.cs
@@ -63,12 +63,22 @@ public class FootSteps : MonoBehaviour {
 	}
 
 	void Footstep(int i){
-		Debug.Log ("Hello! Fot, fotsteg, fot fot, fotsteg steg: " + i);
-		RaycastHit _rayHit;
-		Physics.SphereCast (transform.position + new Vector3 (0, 1, 0), 0.3f, Vector3.down, out _rayHit, 1.1f);
-		GameObject sauce = (GameObject)Instantiate (_audioSauce, GameObject.Find ("L_foot_joint").transform.position, Quaternion.identity);
+		int a = i;
+		if(tag == "Player"){
+			GameObject sauce = (GameObject)Instantiate (_audioSauce, GameObject.Find ("L_foot_joint").transform.position, Quaternion.identity);
+			sauce.audio.clip = GetComponent<FootSteps> ().getRandomClip (a);
+			sauce.audio.Play ();
+		}
+
+		else if(tag == "Fox"){
+			Debug.Log ("FOX STEPS");
+			GameObject sauce = (GameObject)Instantiate (_audioSauce, GameObject.Find ("R_front_foot_joint").transform.position, Quaternion.identity);
+			sauce.audio.clip = GetComponent<FootSteps> ().getRandomClip (a);
+			sauce.audio.Play ();
+		}
+			
+
 		
-		sauce.audio.clip = GetComponent<FootSteps> ().getRandomClip (0);
-		sauce.audio.Play ();
+
 	}
 }
