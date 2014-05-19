f2d84efc3ac4729cfd6a51a1bdcdc9c40afcb763
minewounds <john.gustafsson@hotmail.se>
2014-05-19 14:39:29 +0200

    Smounds!!!

---
+++ Assets/FadeSound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeSound : MonoBehaviour {
+
+	public bool _activated;
+
+	[Range(0f,1f)]
+	public float _fadeSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(_activated)
+			GetComponent<AudioSource>().volume = Mathf.Lerp(GetComponent<AudioSource>().volume, 1, Time.deltaTime * _fadeSpeed);
+		else 
+			GetComponent<AudioSource>().volume = Mathf.Lerp(GetComponent<AudioSource>().volume, 0, Time.deltaTime * _fadeSpeed);
+	
+	}
+
+	public void ChangeState(){
+		_activated = !_activated;
+	}
+
+}
