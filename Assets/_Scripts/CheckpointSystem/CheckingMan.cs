bb7e10d6db9eac8e5126f66122a699fe5d0ff98a
Lajjo <Lajjo1992@gmail.com>
2014-05-22 10:40:21 +0200

    uppgrayde

---
+++ Assets/_Scripts/CheckpointSystem/CheckingMan.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckingMan : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider c){
+
+		Debug.Log(c.tag);
+
+		if(c.CompareTag("Checkpoint")){
+
+
+
+		}
+
+	}
+
+}
