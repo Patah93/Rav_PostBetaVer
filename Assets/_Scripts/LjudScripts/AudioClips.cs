5b1b8f13ada8ce8e974cc6f16a60ca95cb8d9a46
Undertagaren <Undertagaren>
2014-05-29 12:29:35 +0200

    Mucho Soundo

--- Assets/_Scripts/LjudScripts/AudioClips.cs
+++ Assets/_Scripts/LjudScripts/AudioClips.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class AudioClips : MonoBehaviour {
 
-	public AudioClip[] _audios;
+	public AudioClip[]_audios;
+	public AudioClip[] _stoneSounds;
+	public AudioClip[] _sandSounds;
+	public AudioClip[] _metalSounds;
 
 	public AudioClip GetRandomClip(){
-
 		return _audios[Random.Range(0, _audios.Length)];
-
 	}
 
 }
